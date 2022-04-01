20e7918cd8747f748b5d5dee6640017f7845ba5f
Ronaldo Gomes <ronaldosg1999@outlook.com>
2022-04-01 09:20:13 -0300

    test: ensure AccessToken expires in 30 minutes

--- src/domain/models/access-token.ts
+++ src/domain/models/access-token.ts
@@ -1,3 +1,8 @@
 export class AccessToken {
   constructor (private readonly value: string) {}
+
+  static get expirationInMs (): number {
+    const minutes = 30
+    return minutes * 60 * 1000
+  }
 }
